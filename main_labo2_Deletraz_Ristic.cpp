b2f71a8a425a5be44c98fa335513f09023dda1d7
chris4HEIG <114987479+chris4HEIG@users.noreply.github.com>
2022-10-06 14:37:56 +0200

    Mise a jour test

--- main_labo2_Deletraz_Ristic.cpp
+++ main_labo2_Deletraz_Ristic.cpp
@@ -2,9 +2,9 @@
   ---------------------------------------------------------------------------
   Fichier     : main_labo2_Deletraz_Ristic.cpp
   Nom du labo : à compléter
-  Auteur(s)   : Delétraz Alexandre - Ristic Chritopher
+  Auteur(s)   : Delétraz Alexandre - Ristic Christopher
   Date        : 06.10.2022
-  But         : le but du programme et non le but du laboratoire !!
+  But         : Calculer la course d'un taxi
 
   Remarque(s) : à compléter
 
@@ -20,6 +20,10 @@ using namespace std;
 
 
 int main() {
+
+   const float PRISE_EN_CHARGE = 5.0f;
+   const float TAXE_DE_BAGAGE  = 2.0f;
+
    cout << "Bonjour ce programme calculera le prix d'une course de " << endl
         << "Taxi."                                                   << endl;
 
