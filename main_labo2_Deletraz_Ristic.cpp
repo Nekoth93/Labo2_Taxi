de61511e2b235fc41eab32eed771af0555033e7e
chris4HEIG <114987479+chris4HEIG@users.noreply.github.com>
2022-10-06 19:54:05 +0200

    affichage commande pour entree utilisateur

--- main_labo2_Deletraz_Ristic.cpp
+++ main_labo2_Deletraz_Ristic.cpp
@@ -15,7 +15,6 @@
 #include <iostream>   // entrées / sorties
 #include <limits>     // pour vider le buffer
 #include <iomanip>    // gestion de l'affichage
-#include <string>     // affichage de texte type string
 
 using namespace std;
 
@@ -25,6 +24,11 @@ int main()
     const float TAXE_DE_BAGAGE     = 2.0f;
     const float TARIF_MINUTE_JOUR  = 1.0f;
     const float TARIF_MINUTE_NUIT  = 1.6f;
+
+    float nbreBagage;
+    float distanceKm;
+    float vitesseKmh;
+    int   departHeure;
 
     // affichage de la carte de course d'un taxi
 
@@ -36,7 +40,7 @@ int main()
          // la virgule pour les réels
 
          << "- prise en charge \t: " << PRISE_EN_CHARGE                     << endl
-         << "- supp par bagag  \t: " << TAXE_DE_BAGAGE                      << endl
+         << "- supp par bagage \t: " << TAXE_DE_BAGAGE                      << endl
          << "- tarif/min (jour)\t: " << TARIF_MINUTE_JOUR                   << endl
          << "- tarif/min (nuit)\t: " << TARIF_MINUTE_NUIT                   << endl
          << "- heures de jour  \t: [ 8-20]"                                 << endl
@@ -44,13 +48,31 @@ int main()
 
     // affichage et entrées pour la commande
 
-
-
+    cout << "Votre commande" << endl
+         << "==============" << endl
+         << "- nbre de bagage   [0 - 4] : ";
+    cin  >> nbreBagage;
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // vide le buffer
+
+    cout << endl
+         << "- distance [km]    [0 - 500] : ";
+    cin  >> distanceKm;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << endl
+         << "- vitesse [km/h]  [50 - 120] : ";
+    cin  >> vitesseKmh;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << endl
+         << "depart              [0 - 23] : ";
+    cin  >> departHeure;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
 
 
     //TODO si on utilise trop de fois le meme chiffre dans setw(5) on fait un
     // define en haut
-
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
    return EXIT_SUCCESS;
 }
