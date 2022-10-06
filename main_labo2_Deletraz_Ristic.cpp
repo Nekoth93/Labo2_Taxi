ab597c1c11b7a455582a24e2b7709011a0f7a538
Alexandre Delétraz <alexandre.deletraz@hotmail.com>
2022-10-06 14:13:17 +0200

    Ajout en-tête

---
+++ main_labo2_Deletraz_Ristic.cpp
@@ -0,0 +1,29 @@
+/*
+  ---------------------------------------------------------------------------
+  Fichier     : main_labo2_Deletraz_Ristic.cpp
+  Nom du labo : à compléter
+  Auteur(s)   : Delétraz Alexandre - Ristic Chritopher
+  Date        : 06.10.2022
+  But         : le but du programme et non le but du laboratoire !!
+
+  Remarque(s) : à compléter
+
+  Compilateur : gcc version 11.2.0
+  ---------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main() {
+   cout << "Bonjour ce programme calculera le prix d'une course de " << endl
+        << "Taxi."                                                   << endl;
+
+
+
+   return EXIT_SUCCESS;
+}
